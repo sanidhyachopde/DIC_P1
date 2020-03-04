76ab59a2a88c86e6fcad97c7f62dc08780f9dbe5
Sanidhya Chopde <sanidhya.chopde@gmail.com>
2020-03-03 19:19:10 -0500

    First push

---
+++ Predictive_Analytics.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Predicitve_Analytics.py
+"""
+
+
+def Accuracy(y_true,y_pred):
+    """
+    :type y_true: numpy.ndarray
+    :type y_pred: numpy.ndarray
+    :rtype: float
+    
+    """
+
+def Recall(y_true,y_pred):
+     """
+    :type y_true: numpy.ndarray
+    :type y_pred: numpy.ndarray
+    :rtype: float
+    """
+
+def Precision(y_true,y_pred):
+    """
+    :type y_true: numpy.ndarray
+    :type y_pred: numpy.ndarray
+    :rtype: float
+    """
+def WCSS(Clusters):
+    """
+    :Clusters List[numpy.ndarray]
+    :rtype: float
+    """
+def ConfusionMatrix(y_true,y_pred):
+    
+    """
+    :type y_true: numpy.ndarray
+    :type y_pred: numpy.ndarray
+    :rtype: float
+    """  
+
+def KNN(X_train,X_test,Y_train):
+     """
+    :type X_train: numpy.ndarray
+    :type X_test: numpy.ndarray
+    :type Y_train: numpy.ndarray
+    
+    :rtype: numpy.ndarray
+    """
+def RandomForest(X_train,Y_train,X_test):
+    """
+    :type X_train: numpy.ndarray
+    :type X_test: numpy.ndarray
+    :type Y_train: numpy.ndarray
+    
+    :rtype: numpy.ndarray
+    """
+    
+def PCA(X_train,N):
+    """
+    :type X_train: numpy.ndarray
+    :type N: int
+    :rtype: numpy.ndarray
+    """
+    
+def Kmeans(X_train,N):
+    """
+    :type X_train: numpy.ndarray
+    :type N: int
+    :rtype: List[numpy.ndarray]
+    """
+
+def SklearnSupervisedLearning(X_train,Y_train,X_test):
+    """
+    :type X_train: numpy.ndarray
+    :type X_test: numpy.ndarray
+    :type Y_train: numpy.ndarray
+    
+    :rtype: List[numpy.ndarray] 
+    """
+
+def SklearnVotingClassifier(X_train,Y_train,X_test):
+    
+    """
+    :type X_train: numpy.ndarray
+    :type X_test: numpy.ndarray
+    :type Y_train: numpy.ndarray
+    
+    :rtype: List[numpy.ndarray] 
+    """
+
+
+"""
+Create your own custom functions for Matplotlib visualization of hyperparameter search. 
+Make sure that plots are labeled and proper legends are used
+"""
+
+
+
+    
