949d187b5d567361a0351c7be32c491f486a749c
Sanidhya Chopde <sanidhya.chopde@gmail.com>
2020-03-03 19:23:28 -0500

    Aswin's version

--- Predictive_Analytics.py
+++ Predictive_Analytics.py
@@ -2,6 +2,10 @@
 """
 Predicitve_Analytics.py
 """
+import pandas as pd 
+import numpy as np
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 
 def Accuracy(y_true,y_pred):
@@ -37,15 +41,52 @@ def ConfusionMatrix(y_true,y_pred):
     :type y_pred: numpy.ndarray
     :rtype: float
     """  
+    K = len(np.unique(y_true))
+    print(K)
+    cm = np.zeros((K, K))
+    for a, p in zip(y_true, y_pred):
+        cm[a-1][p-1] += 1
+    return cm
+    # also get the accuracy easily with numpy
+    #accuracy = (actual == predicted).sum() / float(len(actual))
 
-def KNN(X_train,X_test,Y_train):
-     """
+def KNN(X_train,X_test,Y_train , k):
+    """
     :type X_train: numpy.ndarray
     :type X_test: numpy.ndarray
     :type Y_train: numpy.ndarray
     
     :rtype: numpy.ndarray
     """
+    num_test = X_test.shape[0]
+    num_train = X_train.shape[0]
+    dists = np.zeros((num_test, num_train))
+    dists = np.sqrt((X_test**2).sum(axis=1)[:, np.newaxis] + (X_train**2).sum(axis=1) - 2 * X_test.dot(X_train.T))
+    num_test = dists.shape[0]
+    y_pred = np.zeros(num_test)
+    for i in range(num_test):
+      # A list of length k storing the labels of the k nearest neighbors to
+      # the ith test point.
+      #closest_y = []
+      #########################################################################
+      # TODO:                                                                 #
+      # Use the distance matrix to find the k nearest neighbors of the ith    #
+      # training point, and use self.y_train to find the labels of these      #
+      # neighbors. Store these labels in closest_y.                           #
+      # Hint: Look up the function numpy.argsort.                             #
+      #########################################################################
+      y_indicies = np.argsort(dists[i, :], axis = 0)
+      closest_y = Y_train[y_indicies[:k]]
+      #########################################################################
+      # TODO:                                                                 #
+      # Now that you have found the labels of the k nearest neighbors, you    #
+      # need to find the most common label in the list closest_y of labels.   #
+      # Store this label in y_pred[i]. Break ties by choosing the smaller     #
+      # label.                                                                #
+      #########################################################################
+      y_pred[i] = np.argmax(np.bincount(closest_y))
+    return y_pred   
+    
 def RandomForest(X_train,Y_train,X_test):
     """
     :type X_train: numpy.ndarray
@@ -77,7 +118,6 @@ def SklearnSupervisedLearning(X_train,Y_train,X_test):
     
     :rtype: List[numpy.ndarray] 
     """
-    print("Hi")
 
 def SklearnVotingClassifier(X_train,Y_train,X_test):
     
@@ -94,6 +134,21 @@ def SklearnVotingClassifier(X_train,Y_train,X_test):
 Create your own custom functions for Matplotlib visualization of hyperparameter search. 
 Make sure that plots are labeled and proper legends are used
 """
+data = pd.read_csv("data.csv")
+X = data.iloc[:, :48].values
+Y = data.iloc[:, 48].values
+#X = (X - np.min(X))/(np.max(X) - np.min(X)) #normalising
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+temp = KNN(X_train,X_test,Y_train , 3)
+temp = temp.astype(np.int64)
+count = 0
+conf = ConfusionMatrix(Y_test,temp)
+print(conf)
+for i in range(temp.shape[0]):
+    if temp[i]==Y_test[i]:
+        count+=1
+print(count/temp.shape[0])
+        
 
 
 
